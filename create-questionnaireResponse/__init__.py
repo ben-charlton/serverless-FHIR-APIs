d84169b726226e29ee75673000d82d4bc0eff193
Ben Charlton <ben.charlton@hotmail.com.au>
2021-08-02 23:19:08 +1000

    start on response httptrigger

--- create-questionnaireResponse/__init__.py
+++ create-questionnaireResponse/__init__.py
@@ -1,24 +1,13 @@
 import logging
-
 import azure.functions as func
+from models.api import post_questionnaireResponse
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    res = post_questionnaireResponse(req.get_json(), path, query)
+    if res:
+        return func.HttpResponse(body="success", status_code=200)
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(body="failed", status_code=500)
