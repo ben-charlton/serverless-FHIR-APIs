0805ac3f1a73079cb0310b854fa731579853e002
Ben Charlton <ben.charlton@hotmail.com.au>
2021-08-02 23:18:37 +1000

    continue on create ques

--- create-questionnaire/__init__.py
+++ create-questionnaire/__init__.py
@@ -5,18 +5,13 @@ import requests
 import json
 import urllib
 import sqlalchemy 
-from models.questionnaire import Questionnaire
+from models.api import post_questionnaire
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    json_dict = req.get_json()
-    created_questionnaire = Questionnaire()
-    created_questionnaire.update_with_json(json_dict)
-    did_save = created_questionnaire.save()
-
-    if did_save:
+    res = post_questionnaire(req.get_json(), path, query)
+    if res:
         return func.HttpResponse(body="success", status_code=200)
     else:
         return func.HttpResponse(body="failed", status_code=500)
